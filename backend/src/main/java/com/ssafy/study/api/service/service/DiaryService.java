9fd6209f3772cf37b8d5599f0b03838bda370486
ParkSeRyeong <sr5871@gmail.com>
2021-09-25 21:19:04 +0900

    Chore: Integrate folder

---
+++ backend/src/main/java/com/ssafy/study/api/service/service/DiaryService.java
@@ -0,0 +1,8 @@
+package com.ssafy.study.api.service.service;
+
+import com.ssafy.study.api.response.DiaryRes;
+import com.ssafy.study.api.response.MyStudyRes;
+
+public interface DiaryService {
+    DiaryRes getDailyDiary();
+}
