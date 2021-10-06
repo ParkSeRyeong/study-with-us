b3b0c3ff395c5f02969e1c0abc00d6f53618af6e
ParkSeRyeong <sr5871@gmail.com>
2021-10-07 03:30:45 +0900

    fix

--- frontend/src/components/TitleBar.vue
+++ frontend/src/components/TitleBar.vue
@@ -8,23 +8,21 @@
 
 <script>
 export default {
-  name: 'TitleBar'
-}
-
+  name: "TitleBar",
+};
 </script>
 
 <style>
-  .title-bar {
-      background-color: #f5ff6971;
-      height: 7vh;
-  }
+.title-bar {
+  background-color: #bceef1;
+  height: 7vh;
+}
 
-  .container {
-    font-size: 25px;
-    font-family: 'NanumBarunGothic-Bold';
-    vertical-align: middle;
-    line-height: 8vh;
-  }
-
-  
-</style> 
+.container {
+  font-size: 7vw;
+  vertical-align: middle;
+  font-family: "IM_Hyemin-Regular";
+  font-weight: 600;
+  line-height: 8vh;
+}
+</style>
