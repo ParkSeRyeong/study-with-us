beb2aeb81c36dc26fb6df6780ea222da1a60bf09
hoyeong-s <71427588+hoyeong-s@users.noreply.github.com>
2021-10-07 03:41:35 +0900

    [fix] selfstudy, groupstudy

--- frontend/src/views/StudyGroup.vue
+++ frontend/src/views/StudyGroup.vue
@@ -1,5 +1,6 @@
 <template>
-  <img src="../assets/images/groupstudy_null.png">
+  <img src="../assets/images/groupstudy_null.png" style="width: 100%; position: absolute;top: 15%;">
+
 </template>
 
 <script>
